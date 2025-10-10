55a4e6a947f54693c9a742c811af8b4f5e0e9b66
Siddharudh Sanmukh <siddusanmukh@gmail.com>
2025-10-10 16:21:32 +0530

    bug fix

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
